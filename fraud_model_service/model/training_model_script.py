5bf0d05d4a6bb444e58d9b71764f18dfa96d5fcf
rezaatae <r.atae@yahoo.com>
2025-06-02 08:02:57 +0100

    model training

---
+++ fraud_model_service/model/training_model_script.py
@@ -0,0 +1,185 @@
+#Basic libraries
+import pandas as pd
+import numpy as np
+import joblib
+
+#preprocessing libraries
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+#ML libraries
+import tensorflow as tf
+# from sklearn.svm import SVC
+# from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+# from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.svm import SVC
+# from sklearn.svm import LinearSVC
+# from sklearn.pipeline import Pipeline
+
+#Metrics Libraries
+# from sklearn import metrics
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import confusion_matrix
+
+
+
+#Misc libraries
+import warnings
+warnings.filterwarnings("ignore")
+
+#Reading the data
+paysim=pd.read_csv("Fraud.csv")
+
+#Tallying the balance
+def balance_diff(data):
+    '''balance_diff checks whether the money debited from sender has exactly credited to the receiver
+       then it creates a new column which indicates 1 when there is a deviation else 0'''
+    #Sender's balance
+    orig_change=data['newbalanceOrig']-data['oldbalanceOrg']
+    orig_change=orig_change.astype(int)
+    for i in orig_change:
+        if i<0:
+            data['orig_txn_diff']=round(data['amount']+orig_change,2)
+        else:
+            data['orig_txn_diff']=round(data['amount']-orig_change,2)
+    data['orig_txn_diff']=data['orig_txn_diff'].astype(int)
+    data['orig_diff'] = [1 if n !=0 else 0 for n in data['orig_txn_diff']] 
+    
+    #Receiver's balance
+    dest_change=data['newbalanceDest']-data['oldbalanceDest']
+    dest_change=dest_change.astype(int)
+    for i in dest_change:
+        if i<0:
+            data['dest_txn_diff']=round(data['amount']+dest_change,2)
+        else:
+            data['dest_txn_diff']=round(data['amount']-dest_change,2)
+    data['dest_txn_diff']=data['dest_txn_diff'].astype(int)
+    data['dest_diff'] = [1 if n !=0 else 0 for n in data['dest_txn_diff']] 
+    
+    data.drop(['orig_txn_diff','dest_txn_diff'],axis=1,inplace = True)
+    
+#Surge indicator
+def surge_indicator(data):
+    '''Creates a new column which has 1 if the transaction amount is greater than the threshold
+    else it will be 0'''
+    data['surge']=[1 if n>450000 else 0 for n in data['amount']]
+
+#Frequency indicator
+def frequency_receiver(data):
+    '''Creates a new column which has 1 if the receiver receives money from many individuals
+    else it will be 0'''
+    data['freq_Dest']=data['nameDest'].map(data['nameDest'].value_counts())
+    data['freq_dest']=[1 if n>20 else 0 for n in data['freq_Dest']]
+    
+    data.drop(['freq_Dest'],axis=1,inplace = True)
+
+#Tracking the receiver as merchant or not
+def merchant(data):
+    '''We also have customer ids which starts with M in Receiver name, it indicates merchant
+    this function will flag if there is a merchant in receiver end '''
+    values = ['M']
+    conditions = list(map(data['nameDest'].str.contains, values))
+    data['merchant'] = np.select(conditions, '1', '0')
+
+#Creating a copy
+paysim_1=paysim.copy()
+
+#Getting the max size
+max_size = paysim_1['isFraud'].value_counts().max()
+
+#Balancing the target label
+lst = [paysim_1]
+for class_index, group in paysim_1.groupby('isFraud'):
+    lst.append(group.sample(max_size-len(group), replace=True))
+paysim_1 = pd.concat(lst)
+
+#One hot encoding
+paysim_1=pd.concat([paysim_1,pd.get_dummies(paysim_1['type'], prefix='type_')],axis=1)
+paysim_1.drop(['type'],axis=1,inplace = True)
+
+#Splitting dependent and independent variable
+paysim_2=paysim_1.copy()
+X=paysim_2.drop('isFraud',axis=1)
+y=paysim_2['isFraud']
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=111)
+
+#Standardizing the numerical columns
+col_names=['amount','oldbalanceOrg','newbalanceOrig','oldbalanceDest','newbalanceDest']
+features_train = X_train[col_names]
+features_test = X_test[col_names]
+scaler = StandardScaler().fit(features_train.values)
+features_train = scaler.transform(features_train.values)
+features_test = scaler.transform(features_test.values)
+X_train[col_names] = features_train
+X_test[col_names] =features_test
+
+#Tokenzation of customer name to get a unique id
+tokenizer_org = tf.keras.preprocessing.text.Tokenizer()
+tokenizer_org.fit_on_texts(X_train['nameOrig'])
+
+tokenizer_dest = tf.keras.preprocessing.text.Tokenizer()
+tokenizer_dest.fit_on_texts(X_train['nameDest'])
+
+# Create tokenized customer lists
+customers_train_org = tokenizer_org.texts_to_sequences(X_train['nameOrig'])
+customers_test_org = tokenizer_org.texts_to_sequences(X_test['nameOrig'])
+
+customers_train_dest = tokenizer_dest.texts_to_sequences(X_train['nameDest'])
+customers_test_dest = tokenizer_dest.texts_to_sequences(X_test['nameDest'])
+
+
+# Pad sequences
+X_train['customers_org'] = tf.keras.preprocessing.sequence.pad_sequences(customers_train_org, maxlen=1)
+X_test['customers_org'] = tf.keras.preprocessing.sequence.pad_sequences(customers_test_org, maxlen=1)
+
+X_train['customers_dest'] = tf.keras.preprocessing.sequence.pad_sequences(customers_train_dest, maxlen=1)
+X_test['customers_dest'] = tf.keras.preprocessing.sequence.pad_sequences(customers_test_dest, maxlen=1)
+
+
+X_train=X_train.drop(['nameOrig','nameDest','isFlaggedFraud'],axis=1)
+X_train = X_train.reset_index(drop=True)
+
+X_test=X_test.drop(['nameOrig','nameDest','isFlaggedFraud'],axis=1)
+X_test = X_test.reset_index(drop=True)
+
+#creating the objects
+# logreg_cv = LogisticRegression(solver='liblinear',random_state=123)
+# dt_cv=DecisionTreeClassifier(random_state=123)
+# knn_cv=KNeighborsClassifier()
+# svc_cv=SVC(kernel='linear',random_state=123)
+# nb_cv=GaussianNB()
+# rf_cv=RandomForestClassifier(random_state=123)
+# cv_dict = {0: 'Logistic Regression', 1: 'Decision Tree',2:'KNN',3:'SVC',4:'Naive Bayes',5:'Random Forest'}
+# cv_models=[logreg_cv,dt_cv,knn_cv,svc_cv,nb_cv,rf_cv]
+nb_cv=GaussianNB()
+cv_dict = {0 :'Naive Bayes'}
+cv_models=[nb_cv]
+
+for i,model in enumerate(cv_models):
+    print("{} Test Accuracy: {}".format(cv_dict[i],cross_val_score(model, X_train, y_train, cv=10, scoring ='accuracy').mean()))
+
+
+param_grid_nb = {
+    'var_smoothing': np.logspace(0,-9, num=100)
+}
+
+nbModel_grid = GridSearchCV(estimator=GaussianNB(), param_grid=param_grid_nb, verbose=1, cv=10, n_jobs=-1)
+nbModel_grid.fit(X_train, y_train)
+print(nbModel_grid.best_estimator_)
+y_pred=nbModel_grid.predict(X_test)
+
+print(classification_report(y_test, y_pred, target_names=['Not Fraud','Fraud']))
+print("reztest joblib dump")
+joblib.dump(nbModel_grid, 'nbModel.joblib')
